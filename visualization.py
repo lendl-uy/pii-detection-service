32e43acdc8be190dc8275777fa55b3f6967dbeca
lendl-uy <lendl13.uy@gmail.com>
2024-04-10 12:43:55 +0800

    Placed all visualization code in a dedicated file

--- visualization.py
+++ visualization.py
@@ -1,5 +1,9 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+from collections import Counter
+
+from constants import *
+from read_dataset import unzip_file, read_pii_json
 
 def visualize_labels(labels, frequencies):
     
@@ -19,7 +23,7 @@ def visualize_labels(labels, frequencies):
     plt.title("Frequency of Each Label", fontsize=16)
 
     # Rotate x-axis labels for better readability
-    plt.xticks(rotation=45, fontsize=12)
+    plt.xticks(rotation=45, fontsize=10)
 
     # Add gridlines for y-axis
     plt.grid(axis="y", linestyle="--", alpha=0.7)
@@ -29,4 +33,31 @@ def visualize_labels(labels, frequencies):
         plt.text(i, freq + 0.5, str(freq), ha="center", va="bottom")
 
     plt.tight_layout()  # Adjust layout to make room for the rotated x-axis labels
+    plt.yscale("log")
     plt.show()
+
+def main():
+    # Since train.json is too large, it was zipped
+    # To read the file, unzip then pass to the json parser   
+    unzip_file(ZIPPED_TRAIN_SET_PATH, "datasets/")
+        
+    _ , texts_train, tokens_train, _ , labels_train = read_pii_json(TRAIN_SET_PATH, is_train=True)
+    _ , texts_test, tokens_test, _ = read_pii_json(TEST_SET_PATH)
+    
+    flat_labels = [label for sublist in labels_train for label in sublist]
+    unique_labels = set(flat_labels)
+    
+    # Count the label frequencies
+    label_counts = Counter(flat_labels)
+
+    # Prepare data for plotting
+    labels, frequencies = zip(*label_counts.items())
+    
+    # Don't include "O", not PIIs
+    # pii_labels = labels[1:]
+    # pii_frequencies = frequencies[1:]
+
+    visualize_labels(labels, frequencies)
+
+if __name__ == "__main__":
+    main()
