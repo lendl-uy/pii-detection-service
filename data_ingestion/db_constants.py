1e3c4ada9267ce9a8bac871a5bd2d6f2dfa0367b
lendl-uy <lendl13.uy@gmail.com>
2024-04-13 11:30:49 +0800

    Updated scripts from mysql to psql

--- data_ingestion/db_constants.py
+++ data_ingestion/db_constants.py
@@ -1,8 +1,8 @@
 # Database configuration constants
-DB_HOST = "pii-detector-staging.c5mceuobywlx.ap-southeast-1.rds.amazonaws.com"
+DB_HOST = "pii-detection-staging.c5mceuobywlx.ap-southeast-1.rds.amazonaws.com"
 DB_USER = "ai231_g1_admin"
 DB_PASS = "ai231ay202324"
-DB_NAME = "pii_detector_staging"
+DB_NAME = "pii_detection_staging"
 
 # Database testing
 PATH_TO_SAMPLE_ESSAY = "../datasets/sample_input.json"
