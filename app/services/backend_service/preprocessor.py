6e06348d4581b40ba60e01eb49fad4d91a8c64b8
lendl-uy <jluy@smartfoxdata.com>
2024-05-26 22:51:35 +0800

    Handle special characters such as \n\n

--- app/services/backend_service/preprocessor.py
+++ app/services/backend_service/preprocessor.py
@@ -1,5 +1,6 @@
 import json
 import re
+import codecs
 
 class Preprocessor:
 
@@ -16,9 +17,12 @@ class Preprocessor:
 
         return self.full_text
 
+    def decode_escapes(self, input_text):
+        # Decode all escape sequences using the unicode_escape codec
+        return codecs.decode(input_text, 'unicode_escape')
+
     def tokenize(self, full_text):
         # Use regular expression to find sequences of word characters, punctuation, special characters, or whitespace.
         # This pattern attempts to keep sequences of non-space special characters intact while splitting other tokens correctly.
-        self.tokens = re.findall(r'\n\n+|\\u[0-9a-fA-F]{4}|[^\w\s]|[\w]+', full_text)
-
+        self.tokens = re.findall(r'\n\n+|\\u[0-9a-fA-F]{4}|[^\w\s]|[\w]+', self.decode_escapes(full_text))
         return self.tokens
